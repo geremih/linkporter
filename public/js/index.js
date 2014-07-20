45dce0ff431f1bcc46cf5ea6ff80f8066497d795
Mihir Rege <mihirrege@gmail.com>
2014-07-20 12:00:48 +0530

    When no user is connected, queue url
    - Move js to separate file

---
+++ public/js/index.js
@@ -0,0 +1,48 @@
+var socket = io.connect();
+var count = 0;
+var url_queue = [];
+
+
+
+$( function (){
+
+    console.log("File loaded");
+    function send_url (url){
+        socket.emit('url transfer',url);
+    }
+
+    
+    $('form').submit(function(){
+        var url =  $('#url').val();
+        if(count === 0){
+            url_queue.push(url);
+        }
+        else{
+            send_url(url);
+            $('#url').val('');
+        }
+        return false;
+    });
+
+    
+    socket.on('url transfer', function(url){
+        window.open(url,"_self");
+    });
+
+    socket.on('count', function(total){
+        console.log("count calleed");
+        count = total -1;
+        $('#count').text(count);
+        if(count > 0){
+            while(url_queue.length !== 0)
+            {
+                send_url(url_queue.pop());
+                $('#url').val('');
+            }
+        }
+    });
+
+
+});
+
+
